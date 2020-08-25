bc58eb3203a4e6e29c6ce9dd0a2e732c77a12187
unknown <mahendrakar987@gmail.com>
2020-08-25 17:42:36 -0400

    lecture 7 add

--- lecture04/app.js
+++ lecture04/app.js
@@ -1,11 +1,27 @@
 (function (){
 
-	//'use strict';
-    x="hello";
+	'use strict';
+    //x="hello";
    angular.module('MyFirstApp',[])
 
-   .controller('MyFirstController',function(){
+   .controller('MyFirstController',function ($scope) {
+           $scope.name="";
+           $scope.totalValue=0;
 
+           $scope.displayNumeric =function () {
+           	var totalNameValue=calculateNumericForString($scope.name);
+           	$scope.totalValue=totalNameValue;
+
+           }
+
+           function calculateNumericForString(string){
+                  var totalStringValue =0;
+                  for (var i=0; i < string.length; i++){
+                  	totalStringValue +=string.charCodeAt(i);
+                  }
+              return totalStringValue;
+
+           }
 
    });
 
